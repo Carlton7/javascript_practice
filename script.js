ffe6a2d4925805959f4579c474815c34e37c74e9
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-01 11:32:46 +0000

    task 2 complete - variables js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 `use strict`;
 
-//let a = 2;
+//TASK 1
 
 console.log("Hello World")
 
@@ -18,3 +18,22 @@ console.error('leo');
 console.log('my favourite car is ' + make + ' and the model is '+ model )
 
 console.log("writing a test run for %c css changes to a statement", "color:green; background-color:orange; font-style: Bold; font-size: 40px; padding:40px");
+
+
+//TASK 2
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${moneyPaidSoFar}`)
