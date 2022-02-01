bd54857a2db98c2ad26f232fb8159d05f86b33be
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-01 15:21:24 +0000

    adding fizzbuzz challenge

--- script.js
+++ script.js
@@ -127,3 +127,20 @@
 //   age2++
 // }  
 
+// CHALLENGE
+
+//FizzBuzz
+
+let n = 1;
+while (n <= 100) {
+  if (n%3 == 0 && n%5 == 0) {
+    console.log("FizzBuzz")
+  } else if (n%5 == 0) {
+    console.log("Buzz")
+  } else if (n%3 == 0) {
+    console.log("Fizz")
+  } else {
+    console.log(n)
+  }
+  n++
+}
