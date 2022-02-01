e1218f5f4551c86f516903b924c4cc06dfa47aae
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-01 10:24:01 +0000

    task 1-3 completed for js

---
+++ script.js
@@ -0,0 +1,20 @@
+`use strict`;
+
+//let a = 2;
+
+console.log("Hello World")
+
+let make = `AC Cobra`;
+let model = 'GT'
+
+console.log('carlton');
+
+console.info('anand');
+
+console.warn('sydney');
+
+console.error('leo');
+
+console.log('my favourite car is ' + make + ' and the model is '+ model )
+
+console.log("writing a test run for %c css changes to a statement", "color:green; background-color:orange; font-style: Bold; font-size: 40px; padding:40px");
